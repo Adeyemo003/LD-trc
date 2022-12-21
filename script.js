ef453b8ba02677e77e7058bb5c2c9f22c77e9428
Daniel ade <106268594+Adeyemo003@users.noreply.github.com>
2022-12-21 23:06:43 +0100

    second change

--- script.js
+++ script.js
@@ -8,11 +8,19 @@ const inputEl = document.getElementById("input-el")
 
 const deleteBtn = document.querySelector("#delete-btn")
 
+const saveBtn = document.querySelector("#save-btn")
+
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+// this is how the tabs template of chrome is constructed, the urls are objects stored in an array called tabs. so 
+// literally they can be located by the array index
+// const tabs = [
+//   {url: "https://linkedin.com/in/adeyemo-opeyemi-374122243"}
+// ]
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage
-  renderList()
+  renderList(myLeads)
   console.log(myLeads)
   console.log("let")
 }
@@ -20,11 +28,34 @@ else {
   console.log("no")
 }
 
+saveBtn.addEventListener("click", function(){
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderList(myLeads)
+    console.log(tabs[0].url)
+  })
+})
+
+function renderList(Leads) { 
+  listItems = ""
+  for (i=0; i<Leads.length; i++) {
+    listItems += `
+       <li>
+           <a target='_blank' href='${Leads[i]}'> ${Leads[i]} </a>
+       </li>
+   `
+}
+
+ ulEl.innerHTML = listItems 
+}
+
+
 deleteBtn.addEventListener("click", function() {
   myLeads = []
   localStorage.clear()
   ulEl.innerHTML = ""
-  renderList()
+  renderList(myLeads)
   console.log("gone")
 })
 
@@ -34,18 +65,5 @@ btn.addEventListener("click" , function() {
    inputEl.value = ""
    localStorage.setItem("myLeads", JSON.stringify(myLeads))
    console.log(localStorage.getItem("myLeads"))
-   renderList()
+   renderList(myLeads)
 })
-
-function renderList() { 
-   listItems = ""
-   for (i=0; i<myLeads.length; i++) {
-     listItems += `
-        <li>
-            <a target='_blank' href='${myLeads[i]}'> ${myLeads[i]} </a>
-        </li>
-    `
-}
-
-  ulEl.innerHTML = listItems 
-}
